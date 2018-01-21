95007a94f6e366d9e356f81ea862ccb362319c54
mishadokter <mishad050@gmail.com>
2018-01-21 22:34:11 +0100

    Update of index.php

--- index.php
+++ index.php
@@ -1,19 +1,29 @@
 <?php // Head 
 	include 'inc/head.php';
-	include 'inc/nav.php';
 	include 'models/WeatherDataModel.php';
 ?>
-<body>
-
-<p> Body hi </p>
-
-<!-- Body here -->
-<?php
-	$interpreter = new WeatherDataModel('data.dat');
-	var_dump($interpreter->getMultipleStationData(["080290", "252820"]));
-
-?>
-
-
+<body class="wrapper">
+  
+  <div class="header">
+	<a href="index.php"><img src="img/Dutchsat.png" alt="Not found"/></a>
+  </div>
+  
+  <?php include 'inc/nav.php'; ?>
+	  <div class="footer">
+		<h1 class="pageTitle">Lorem Ipsum</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nulla massa, finibus id urna quis, mattis consectetur tortor. 
+		Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed lobortis metus vel pretium sagittis.
+		Vestibulum consectetur arcu mi, a pulvinar nisi consequat id. Aliquam erat volutpat. Donec sagittis dui tortor, non convallis augue vehicula vitae. 
+		Nulla ante ligula, aliquam eu laoreet vel, consectetur nec ante. <br> <br> Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
+		Aenean pretium massa eu metus rhoncus, et facilisis risus bibendum. Nulla aliquam viverra est, quis ornare nisl lobortis et. 
+		Duis in eros at leo bibendum pellentesque eget non metus. 
+		Sed pellentesque nunc diam, ac aliquet turpis rhoncus eu. Vestibulum molestie nunc a sem placerat faucibus vitae quis dolor.</p>
+	  </div>
+  
+	<?php
+		$interpreter = new WeatherDataModel('data.dat');
+		var_dump($interpreter->getMultipleStationData(["080290", "252820"]));
+	?>
+  
 </body>
 </html>

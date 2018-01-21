69a457aa15b62289a880ef98480b0e4f3086a0d7
mishadokter <mishad050@gmail.com>
2018-01-21 22:53:48 +0100

    Added Login page to the navbar

--- inc/nav.php
+++ inc/nav.php
@@ -1,15 +1,15 @@
 <!-- Nav bar -->
   <ul class="navigation">
-    <li><a href="" title="#">Lorem</a></li>
-    <li><a href="" title="#">Ipsum</a></li>
-    <li><a href="" title="#">Dolor</a>
+    <li><a href="#" title="">Lorem</a></li>
+    <li><a href="#" title="">Ipsum</a></li>
+    <li><a href="#" title="">Dolor</a>
       <ul>
-        <li><a href="" title="#">Foo</a></li>
-        <li><a href="" title="#">Bar</a></li>
-        <li><a href="" title="#">Foobar</a></li>
-        <li><a href="" title="#">DutchSat</a></li>
+        <li><a href="#" title="">Foo</a></li>
+        <li><a href="#" title="">Bar</a></li>
+        <li><a href="#" title="">Foobar</a></li>
+        <li><a href="#" title="">DutchSat</a></li>
       </ul>
     </li>
-    <li><a href="" title="#">Sit</a></li>
+    <li><a href="login.php" title="#">Login</a></li>
     <div class="clear"></div>
   </ul>

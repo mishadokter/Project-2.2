139ba603fc2b7950c864981965f2b481459cd0f7
mishadokter <mishad050@gmail.com>
2018-01-23 00:27:30 +0100

    Update of the LoginHandler, works now

--- controllers/logincontroller.php
+++ controllers/logincontroller.php
@@ -1,11 +1,28 @@
 <?php
-include 'inc/database.php';
-
-	if(isset($_POST['username']) && isset($_POST['password'])
+include '../inc/database.php';
+	if(isset($_POST['username']) && isset($_POST['password']))
 	{
 		$username = $_POST['username'];
 		$password = $_POST['password'];
-
+		
+		$query 	  = "SELECT id, username, password, level FROM users WHERE username='$username' AND password='$password'";
+		$result   = mysqli_query($db, $query) or die (mysqli_error($db));
+		$count    = mysqli_num_rows($result);
+		$row	  = mysqli_fetch_assoc($result);
+		$level	  = $row["level"];
+		
+		// store session data
+		@session_start();
+		$_SESSION['login'] = true;
+		$_SESSION['level'] = $level;
+		
+		if ($level == 1) {
+			header("location: ../level1.php");
+		}
+		
+		else {
+			echo "Invalid username and/or password";
+		}
 		
 	}
 ?>

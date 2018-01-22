fdc4f2cdf4d42e7e23b01db48c50007fbfbc8f6e
mishadokter <mishad050@gmail.com>
2018-01-23 00:29:56 +0100

    Improvement of the head

--- inc/head.php
+++ inc/head.php
@@ -2,3 +2,10 @@
 <head>
 	<link href="css/style.css" rel="stylesheet" type="text/css" media="all">
 </head>
+
+<body class="wrapper">
+  
+  <div class="header">
+	<a href="index.php"><img src="img/Dutchsat.png" alt="Not found"/></a>
+  </div>
+<!-- Nav bar -->

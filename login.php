1c79c4859607a9486a44a32df61eb084ed1307c9
mishadokter <mishad050@gmail.com>
2018-01-23 00:31:11 +0100

    Improved the login form

--- login.php
+++ login.php
@@ -2,23 +2,14 @@
 	include 'inc/head.php';
 	include 'inc/nav.php';
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>UNWDMI | Login</title>
-</head>
-<body>
-	<div class='logincontainer'>
-		<form action="controllers/loginhandler.php" method="POST">
-			<label for="username">Username: </label>
-			<input type="text" name="username">
-			<br>
-			<label for="password">Password: </label>
-			<input type="password" name="password">
-			<br>
-			<input type="submit" value="Login">
-		</form>
+	<div class="login-page">
+		<div class="form">
+			<form action="controllers/loginhandler.php" method="POST">
+				<input type="text" name="username" placeholder="Username" required>
+				<input type="password" name="password" placeholder="Password" required>
+				<input type="submit" name="login" class="submitForm" value="Login">
+			</form>
+		</div>
 	</div>
 </body>
 </html>

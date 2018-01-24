17bd61279898a7709287e37309b6350472809352
mishadokter <mishad050@gmail.com>
2018-01-24 13:49:48 +0100

    Update of the nav
    Added login and logout buttons according to the session state

--- inc/nav.php
+++ inc/nav.php
@@ -1,6 +1,11 @@
 <!-- Nav bar -->
+<?php
+@session_start();
+//error_reporting(0);
+error_reporting( error_reporting() & ~E_NOTICE ); //Hides the "variable not defined" error
+?>
   <ul class="navigation">
-    <li><a href="#" title="">Lorem</a></li>
+    <li><a href="index.php" title="">Home</a></li>
     <li><a href="#" title="">Ipsum</a></li>
     <li><a href="#" title="">Dolor</a>
       <ul>
@@ -10,6 +15,13 @@
         <li><a href="#" title="">DutchSat</a></li>
       </ul>
     </li>
-    <li><a href="login.php" title="#">Login</a></li>
+	<?php
+	if(!$_SESSION['login']) { ?>
+		<li><a href="login.php">Login</a></li>
+	<?php 
+	}
+	else { ?>
+		<li><a href="login.php?logout">Logout</a></li>
+	<?php } ?>
     <div class="clear"></div>
   </ul>

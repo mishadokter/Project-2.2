a7c1b4af3fd60a001a010c065ac47bad547dfa78
mishadokter <mishad050@gmail.com>
2018-01-24 14:30:34 +0100

    Update of nav.php
    Added 2 separate navigations according to login/logout state

--- inc/nav.php
+++ inc/nav.php
@@ -4,24 +4,29 @@
 //error_reporting(0);
 error_reporting( error_reporting() & ~E_NOTICE ); //Hides the "variable not defined" error
 ?>
-  <ul class="navigation">
-    <li><a href="index.php" title="">Home</a></li>
-    <li><a href="#" title="">Ipsum</a></li>
-    <li><a href="#" title="">Dolor</a>
-      <ul>
-        <li><a href="#" title="">Foo</a></li>
-        <li><a href="#" title="">Bar</a></li>
-        <li><a href="#" title="">Foobar</a></li>
-        <li><a href="#" title="">DutchSat</a></li>
-      </ul>
-    </li>
 	<?php
-	if(!$_SESSION['login']) { ?>
-		<li><a href="login.php">Login</a></li>
+	if($_SESSION['login']) { ?>
+	<ul class="navigation">
+		<li><a href="index.php" title="">Home</a></li>
+		<li><a href="#" title="">Ipsum</a></li>
+		<li><a href="#" title="">Graphs</a>
+		  <ul>
+			<li><a href="#" title="">Foo</a></li>
+			<li><a href="#" title="">Bar</a></li>
+			<li><a href="#" title="">Foobar</a></li>
+			<li><a href="#" title="">Line graph</a></li>
+		  </ul>
+		</li>		
+		<li><a href="login.php?logout">Logout</a></li>
+		<div class="clear"></div>
+	</ul>
 	<?php 
 	}
 	else { ?>
-		<li><a href="login.php?logout">Logout</a></li>
+	<ul class="navigation">
+		<li><a href="index.php" title="">Home</a></li>
+		<li><a href="#" title="">About</a></li>
+		<li><a href="login.php">Login</a></li>
+		<div class="clear"></div>
+	</ul>
 	<?php } ?>
-    <div class="clear"></div>
-  </ul>

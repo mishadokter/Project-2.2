6bc0e8152a9fe42f8c3fa9ff75852ce97cd9efe1
mishadokter <mishad050@gmail.com>
2018-01-24 13:53:30 +0100

    Added sessions and improvements
    This page is now only accessible when logged in

--- level1.php
+++ level1.php
@@ -1,10 +1,12 @@
 <?php // Head 
 	include 'inc/head.php';
 	include 'inc/database.php';
-	// include 'models/WeatherDataModel.php';
-?>
-  
-  <?php include 'inc/nav.php'; ?>
+	
+	// User must be logged in to acces this page
+	@session_start();
+	if($_SESSION['login']) {
+	include 'inc/nav.php'; ?>
+	
 	  <div class="footer">
 		<h1 class="pageTitle">Lorem Ipsum</h1>
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nulla massa, finibus id urna quis, mattis consectetur tortor. 
@@ -13,17 +15,19 @@
 		Nulla ante ligula, aliquam eu laoreet vel, consectetur nec ante. <br> <br> Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
 		Aenean pretium massa eu metus rhoncus, et facilisis risus bibendum. Nulla aliquam viverra est, quis ornare nisl lobortis et. 
 		Duis in eros at leo bibendum pellentesque eget non metus. 
-		Sed pellentesque nunc diam, ac aliquet turpis rhoncus eu. Vestibulum molestie nunc a sem placerat faucibus vitae quis dolor.</p>
+		Sed pellentesque nunc diam, ac aliquet turpis rhoncus eu. Vestibulum molestie nunc a sem placerat faucibus vitae quis dolor.<br><br>
+		Our weatherservice contains 230 countries/regions and 8000 weather stations </p>
 	  </div>
+	  
 	  <form action="" method="POST">
-	  <select name="selected_country" style='font-family: "Roboto", sans-serif; outline: 0; background: #f2f2f2; width: 100%; border: 150px; margin: 0 0 15px; padding: 15px; box-sizing: border-box; font-size: 14px'>
-
+	  <select name="selected_country" style='font-family: "Roboto", sans-serif; outline: 0; background: #f2f2f2; width: 100%; border: 150px; margin: 0 0 15px; 
+	  padding: 15px; box-sizing: border-box; font-size: 14px'required>
+		<option value=""> --- Choose a country or region --- </option>
             <?php 
 			$query= $db->query("SELECT country from stations GROUP BY country");
              while ($rows = $query->fetch_array(MYSQLI_ASSOC)) {
-                    $value= $rows['country'];
-                ?>
-                 <option value="<?= $value?>"><?= $value?></option>
+                $value= $rows['country']; ?>
+                <option value="<?= $value?>" ><?= $value?></option>
                 <?php } ?>
              </select>
 				<input type="submit" name="login" class="selected_Country" value="Submit!">
@@ -34,6 +38,11 @@
 			$selected_country = $_POST['selected_country'];
 			echo "Chosen country: $selected_country";
 		}
+	} // End login
+	
+	else {
+		header("location: login.php");
+	}
 	?>
   
 </body>

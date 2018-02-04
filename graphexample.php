4065c6fb74339a71bc3d545fa8171efc4428d983
mishadokter <mishad050@gmail.com>
2018-02-05 00:34:00 +0100

    Fetch data from select tag
    Fetch the data from the dashboard page

--- graphexample.php
+++ graphexample.php
@@ -6,13 +6,16 @@
 	@session_start();
 	if($_SESSION['login']) {
 	include 'inc/nav.php'; 
+	
+	// Fetch data from select tag
+	$selected_country = mysqli_real_escape_string($db, $_POST['selected_country']);
 ?>
 	
 	  <div class="footer">
-		<h1 class="pageTitle">Graph example</h1>
+		<h1 class="pageTitle">Graph example of <?php echo "$selected_country";?> </h1>
 		<canvas id="testchart"></canvas>
 		<script>
-			$.getJSON("/Project-2.2/api/test.php", function(result){
+			$.getJSON("test.php", function(result){
 				console.log("Resultl " +result);
 				var ctx = $("#testchart");
 				var myChart = new Chart(ctx, {
@@ -46,8 +49,10 @@
 			)});
 		</script>
 	</body>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.bundle.min.js"></script> 
 </html>
-
+ 
 <?php
 	} // End login
 	
